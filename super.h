1fbeb8d247f8fd11e423dc4dc78a01b44251b43e
hackercracker5 <81564333+hackercracker5@users.noreply.github.com>
2022-03-13 23:09:17 +0200

    Add files via upload

---
+++ super.h
@@ -0,0 +1,15 @@
+#pragma once
+#define vaska std::cout<<"Vaska e temerut"
+#define цч int
+#define двоен double
+#define нишка std::string
+#define плаващ float
+#define ако if
+#define иначе else
+#define за for
+#define докато while
+#define прави  do 
+#define посока std::vector
+#define превключвател switch
+#define квътре std::cin
+#define квънка std::cout
